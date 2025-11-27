31fc3d549000180700d77fbb702215093ffc75e1
Carlos Wylie <cgarciawylie@gmail.com>
2025-11-27 21:57:59 +0100

    programmatic navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,7 +1,21 @@
 <script setup>
+import { useRoute, useRouter } from 'vue-router';
+
 const props = defineProps(["event"])
+
+const router  = useRouter()
+
+const register = () => {
+    // not going to focus on api call but rather assume it is successful
+
+    // programmatic navigation
+    router.push({
+        name : 'EventDetails'
+    })
+}
 </script>
 
 <template>
     <p>Register for the event here.</p>
+    <button @click="register"> Register Me!</button>
 </template>
